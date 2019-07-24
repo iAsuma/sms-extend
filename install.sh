c049b7238154f6b9774b76486b8b4543844dbcc9
iAsuma <770878450@qq.com>
2019-07-24 10:20:50 +0800

    some

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+echo '拷贝源文件至tp extend目录'
 cp -r sms-extend/aliyun extend/
-rm -rf sms-extend/
+cp sms-extend/README.md extend/aliyun/sms.md
+echo '删除源文件'
+rm -r sms-extend/
+echo '执行成功'
